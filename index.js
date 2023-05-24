cf4f56433f8215a266723347c91bb5921a3a7499
Yonela Johannes <johannesyonela@gmail.com>
2023-05-24 03:49:50 +0200

    Chore: move listener into mongoose method

--- index.js
+++ index.js
@@ -62,7 +62,5 @@ app.use('/api/extended_plays', epsRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log("db connected"))
+  .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
   .catch((err) => console.log(err.message));
-
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
